633abe1372a810a086e2440cfb7299147d7099c9
GyanendraChaubey <gyanendrachaubey68@gmai.com>
2022-06-12 14:59:44 +0530

    Add files

--- app.py
+++ app.py
@@ -2,10 +2,11 @@ from flask import Flask
 
 app=Flask(__name__)
 
-@app.route("/", methods=['GET','POST'])
 
+@app.route("/",methods=['GET','POST'])
 def index():
-    return "Starting Machine Learning Project"
+    return "CI CD pipeline has been established."
 
-if __name__ =="__main__":
+
+if __name__=="__main__":
     app.run(debug=True)
